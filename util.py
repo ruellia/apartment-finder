e1a16adbf3779b7c3937803ad81ec8190f908a75
Ruellia Zhang <syncyes@gmail.com>
2017-03-31 13:02:52 -0700

    simple error handling for slack client posting

--- util.py
+++ util.py
@@ -35,11 +35,17 @@ def post_listing_to_slack(sc, listing):
     :param sc: A slack client.
     :param listing: A record of the listing.
     """
-    desc = "{0} | {1} | {2} | {3} | <{4}>".format(listing["area"], listing["price"], listing["bart_dist"], listing["name"], listing["url"])
-    sc.api_call(
-        "chat.postMessage", channel=settings.SLACK_CHANNEL, text=desc,
-        username='pybot', icon_emoji=':robot_face:'
-    )
+    try:
+        desc = "{0} | {1} | {2} | {3} | <{4}>".format(listing["area"], listing["price"], listing["bart_dist"], listing["name"], listing["url"])
+        sc.api_call(
+            "chat.postMessage", channel=settings.SLACK_CHANNEL, text=desc,
+            username='pybot', icon_emoji=':robot_face:'
+        )
+    except:
+        sc.api_call(
+            "chat.postMessage", channel=settings.SLACK_CHANNEL, text="an unexpected error has occurred",
+            username='pybot', icon_emoji=':robot_face:'
+        )
 
 def find_points_of_interest(geotag, location):
     """
