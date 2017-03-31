55455a4921aa1a536a8e41d3ccd5a800738da115
Ruellia Zhang <syncyes@gmail.com>
2017-03-31 13:04:24 -0700

    read basic settings from env var json file path

--- settings.py
+++ settings.py
@@ -1,18 +1,18 @@
 import os
+import json
+
+json_path = os.getenv('JSON_SETTINGS', "")
+
+with open(json_path, 'r') as f:
+    config = json.load(f)
 
 ## Price
 
 # The minimum rent you want to pay per month.
-MIN_PRICE = 1500
+MIN_PRICE = config.get('min_price')
 
 # The maximum rent you want to pay per month.
-MAX_PRICE = 2000
-
-# The minimum number of bedrooms.
-BEDROOMS = 3
-
-# The minimum number of bathrooms.
-BATHROOMS = 1
+MAX_PRICE = config.get('max_price')
 
 ## Location preferences
 
@@ -24,20 +24,16 @@ CRAIGSLIST_SITE = 'sfbay'
 # What Craigslist subdirectories to search on.
 # For instance, https://sfbay.craigslist.org/eby/ is the East Bay, and https://sfbay.craigslist.org/sfc/ is San Francisco.
 # You only need the last three letters of the URLs.
-AREAS = ["eby", "sfc", "sby", "nby"]
+AREAS = ["eby", "sby", "nby"]
 
 # A list of neighborhoods and coordinates that you want to look for apartments in.  Any listing that has coordinates
 # attached will be checked to see which area it is in.  If there's a match, it will be annotated with the area
 # name.  If no match, the neighborhood field, which is a string, will be checked to see if it matches
 # anything in NEIGHBORHOODS.
 BOXES = {
-    "adams_point": [
-        [37.80789, -122.25000],
-        [37.81589,	-122.26081],
-    ],
-    "piedmont": [
-        [37.82240, -122.24768],
-        [37.83237, -122.25386],
+    "albany": [
+        [37.898925, -122.373782],
+        [37.866726, -122.281639],
     ],
     "rockridge": [
         [37.83826, -122.24073],
@@ -51,29 +47,13 @@ BOXES = {
         [37.86425, -122.26330],
         [37.87655, -122.28974],
     ],
-    "pac_heights": [
-        [37.79124, -122.42381],
-        [37.79850, -122.44784],
-    ],
-    "lower_pac_heights": [
-        [37.78554, -122.42878],
-        [37.78873, -122.44544],
-    ],
-    "haight": [
-        [37.77059, -122.42688],
-        [37.77086, -122.45401],
-    ],
-    "sunset": [
-        [37.75451, -122.46422],
-        [37.76258, -122.50825],
+    "oakland": [
+        [37.885438, -122.355881],
+        [37.631714, -122.114793],
     ],
     "richmond": [
         [37.77188, -122.47263],
         [37.78029, -122.51005],
-    ],
-    "presidio": [
-        [37.77805, -122.43959],
-        [37.78829, -122.47151],
     ]
 }
 
@@ -81,7 +61,7 @@ BOXES = {
 # one of the boxes you defined, it will be checked to see if the neighborhood name it was listed under matches one
 # of these.  This is less accurate than the boxes, because it relies on the owner to set the right neighborhood,
 # but it also catches listings that don't have coordinates (many listings are missing this info).
-NEIGHBORHOODS = ["berkeley north", "berkeley", "rockridge", "adams point", "oakland lake merritt", "cow hollow", "piedmont", "pac hts", "pacific heights", "lower haight", "inner sunset", "outer sunset", "presidio", "palo alto", "richmond / seacliff", "haight ashbury", "alameda", "twin peaks", "noe valley", "bernal heights", "glen park", "sunset", "mission district", "potrero hill", "dogpatch"]
+NEIGHBORHOODS = ["berkeley north", "berkeley", "rockridge", "adams point", "oakland lake merritt", "cow hollow", "piedmont", "pac hts", "pacific heights", "lower haight", "palo alto", "richmond / seacliff", "haight ashbury", "alameda", "twin peaks", "noe valley", "bernal heights", "glen park", "potrero hill", "dogpatch"]
 
 ## Transit preferences
 
@@ -95,7 +75,8 @@ TRANSIT_STATIONS = {
     "macarthur_bart": [37.8265657,-122.2686705],
     "rockridge_bart": [37.841286,-122.2566329],
     "downtown_berkeley_bart": [37.8629541,-122.276594],
-    "north_berkeley_bart": [37.8713411,-122.2849758]
+    "north_berkeley_bart": [37.8713411,-122.2849758],
+    "el_cerrito_plaza_bart": [37.902694, -122.298968]
 }
 
 ## Search type preferences
@@ -117,8 +98,12 @@ SLEEP_INTERVAL = 20 * 60 # 20 minutes
 SLACK_CHANNEL = "#housing"
 
 # The token that allows us to connect to slack.
-# Should be put in private.py, or set as an environment variable.
-SLACK_TOKEN = os.getenv('SLACK_TOKEN', "")
+SLACK_TOKEN = config.get('slack_token')
+
+# ruellia's experiments
+BEDROOMS = config.get('bed')
+
+BATHROOMS = config.get('bath')
 
 # Any private settings are imported here.
 try:
